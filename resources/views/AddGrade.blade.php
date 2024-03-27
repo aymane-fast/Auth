e77d04aa34112c1c6515aaea6b29d84899c9115b
aymane_B <aymanefast@gmail.com>
2024-03-27 23:59:20 +0000

    fix add Grad

--- resources/views/AddGrade.blade.php
+++ resources/views/AddGrade.blade.php
@@ -1,21 +1,30 @@
-<form action="{{ route('grades.store') }}" method="POST">
-    @csrf
-    <label for="user_id">Student Name:</label>
-    <select name="user_id" id="user_id">
-        @foreach ($students as $student)
-            <option value="{{ $student->id }}">{{ $student->name }}</option>
-        @endforeach
-    </select>
+<div class="container mt-5">
+    <h2>Add Grade</h2>
+    <form action="{{ route('grades.store') }}" method="POST" class="mt-4">
+        @csrf
+        <div class="form-group">
+            <label for="user_id">Student Name:</label>
+            <select name="user_id" id="user_id" class="form-control">
+                @foreach ($students as $student)
+                    <option value="{{ $student->id }}">{{ $student->name }}</option>
+                @endforeach
+            </select>
+        </div>
 
-    <label for="module_id">Module:</label>
-    <select name="module_id" id="module_id">
-        @foreach ($modules as $module)
-            <option value="{{ $module->id }}">{{ $module->name }}</option>
-        @endforeach
-    </select>
+        <div class="form-group">
+            <label for="module_id">Module:</label>
+            <select name="module_id" id="module_id" class="form-control">
+                @foreach ($modules as $module)
+                    <option value="{{ $module->id }}">{{ $module->name }}</option>
+                @endforeach
+            </select>
+        </div>
 
-    <label for="value">Grade:</label>
-    <input type="number" id="value" name="value" min="0" max="100" required>
+        <div class="form-group">
+            <label for="value">Grade:</label>
+            <input type="number" id="value" name="value" min="0" max="100" required class="form-control">
+        </div>
 
-    <button type="submit">Add Grade</button>
-</form>
+        <button type="submit" class="btn btn-primary">Add Grade</button>
+    </form>
+</div>
